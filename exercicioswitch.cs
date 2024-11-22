f1f4e4bf6da44107720d0d9fdadc63cf4a5d291e
Clara-Noah <claraclaudio806@gmail.com>
2024-11-22 12:54:58 +0000

    Add files via upload

---
+++ exercicioswitch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Exemplo
+{
+    static void Main(string[] args)
+    {
+
+        int nota = 49;
+        switch (nota)
+        {
+            case int n when (n >= 90):
+                Console.WriteLine("classificação final = A");
+                break;
+            case int n when (n >= 80):
+                Console.WriteLine("classificação final = B");
+                break;
+            case int n when (n >= 70):
+                Console.WriteLine("Clasificação final = C");
+                break;
+            case int n when (n >= 60):
+                Console.WriteLine("Classificação final = D");
+                 break;
+            default:
+                Console.WriteLine("Classificação final = F");
+                break;
+           
+
+
+        }
+    }
+}
+
