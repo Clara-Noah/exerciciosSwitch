0a354bd52f6da62b10f04a249ee558c74d3940e6
Clara-Noah <claraclaudio806@gmail.com>
2024-11-22 13:01:14 +0000

    Add files via upload

---
+++ exercicioswitch2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class exercicio7
+{
+    static void Main(string[] args)
+    {
+
+        int dias = 7;
+        switch (dias)
+        {
+            case 1:
+                Console.WriteLine("domingo");
+                break;
+            case 2:
+                Console.WriteLine("segunda");
+                break;
+            case 3:
+                Console.WriteLine("terça");
+                break;
+            case 4:
+                Console.WriteLine("quarta");
+                break;
+            case 5:
+                Console.WriteLine("quinta");
+                break;
+            case 6:
+                Console.WriteLine("sexta");
+                break;
+            default:
+                Console.WriteLine("sabado");
+                break;
+        }
+    }
+}
+
